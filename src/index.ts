27ec306a739656479813f050de14b61a831a4b01
hro19 <fourbasic19@gmail.com>
2024-01-11 20:44:29 +0900

    kvにオブジェクト型配列型をpost,putするときはJSON.stringifyをする、getするときはJSON.parseをする

--- src/index.ts
+++ src/index.ts
@@ -13,18 +13,18 @@ const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/api/items/aa', async (c) => {
   // MY_KV から get
-  const item = await c.env.MY_KV.get('aa')
+  const item:any = await c.env.MY_KV.get('aa')
   console.log(c.env.MY_KV);
-  return c.json({success: true, aa: item})
+  const objitem = JSON.parse(item);
+  return c.json({ success: true, aa: objitem });
 })
 
 app.post('/api/items', async (c) => {
-  const { value } = await c.req.json()
-  if (typeof value === "string") {
-    // MY_KV へ put
-    await c.env.MY_KV.put("aa", value)
-  }
-  return c.json({success: true})
+  const { value } = await c.req.json();
+  // オブジェクトをJSON文字列に変換
+  const jsonvalue = JSON.stringify(value);
+  await c.env.MY_KV.put("aa", jsonvalue);
+  return c.json({ success: true });
 })
 
 export default app
